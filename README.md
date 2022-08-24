47a65aba1c32ccbd2fe1814b7ebbf47fe0e06a84
Th-Axel96 <axelmoussart77@hotmail.com>
2022-08-24 11:32:17 +0200

    push to github

---
+++ README.md
@@ -0,0 +1,18 @@
+# Attribution des tâches (administratif, design)
+
+- Warren :
+  Création base du projet
+  Conception des logos
+  Conception des images(svg, png)
+
+- Axel :
+  Création du README
+  Conception charte graphique
+  Création Wireframe page d'acceuil (desktop)
+
+- Norman :
+  Cahier des charges
+  Réfferencer les concurents
+  Aborescence du site
+
+- Alexandre :
--- src/app.js
+++ src/app.js
@@ -1,20 +1,49 @@
-import _ from "lodash";
 import "./assets/css/styles.css";
-import Icon from './assets/img/img-test-webpack.jpg';
+// import _ from "lodash";
+// import Icon from './assets/img/img-test-webpack.jpg';
 
-function component() {
-  const element = document.createElement('div');
+// function component() {
+//   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['', ''], ' ');
-  element.classList.add("text-3xl");
+//   // Lodash, currently included via a script, is required for this line to work
+//   element.innerHTML = _.join(['', ''], ' ');
+//   element.classList.add("text-3xl");
 
-  const myIcon = new Image();
-  myIcon.src = Icon;
+//   const myIcon = new Image();
+//   myIcon.src = Icon;
 
-  element.appendChild(myIcon);
+//   element.appendChild(myIcon);
 
-  return element;
+//   return element;
+// }
+
+// document.body.appendChild(component());
+
+// dark mod
+
+let button = document.getElementById("dark-mode");
+let body = document.querySelector("body");
+let p = document.querySelector("p")
+
+
+function darkMode() {
+    // alert("ok");
+    body.classList.toggle("bg-black");
+    body.classList.toggle("text-white");
+
 }
 
-document.body.appendChild(component());
+button.addEventListener("click", darkMode);
+
+
+
+let buttonblue = document.getElementById("blue-mode");
+let bodyblue = document.querySelector("body");
+
+function blueMode() {
+    // alert("ok");
+    body.classList.toggle("bg-blue-500");
+    bodyblue.classList.toggle("text-pink-500");
+}
+
+buttonblue.addEventListener("click", blueMode)
