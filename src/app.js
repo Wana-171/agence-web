a74c3182694fbefb4a0a92d66687aeecc9282eb8
warren jarron <98108022+warrenjarron@users.noreply.github.com>
2022-10-04 16:50:00 +0200

    update

--- src/app.js
+++ src/app.js
@@ -56,6 +56,10 @@ import  "@fortawesome/fontawesome-free/js/all";
 // ** For homepage header **
 // ** menu burger for tablet & mobile **
 
+
+
+// ** MENU BURGER **
+
 const btnHomeBurger = document.getElementById("btn-home-burger");
 const menuHomeNav = document.getElementById("menu-home-nav");
 
@@ -65,20 +69,54 @@ const ShowHomeMenu = () => {
 
 btnHomeBurger.addEventListener("click", ShowHomeMenu);
 
-// ** darkmod **
+// ************* darkmod *************
 
 const body = document.querySelector("body");
+
+// for dark mod
 const btnDarkMode1 = document.getElementById("btn-dark-mode1");
 const btnDarkMode2 = document.getElementById("btn-dark-mode2");
 
+// for shadow headet & main
+const shadowLight1 = document.getElementById("shadow-light1");
+const shadowLight2 = document.getElementById("shadow-light2");
+const shadowLightAll = document.querySelectorAll("div.shadow-light-all");
+
+// for shadow mobile header
+const mobileShadowLight1 = document.getElementById("mobile-shadow-light1");
+
+// for bg nav burger
+const darkBurger = document.getElementById("dark-burger");
+
+// for box contact remove bg
+const removeBoxPink = document.querySelectorAll("div.remove-box-pink");
+
 const darkMode = () => {
   body.classList.toggle("bg-black");
+
   body.classList.toggle("text-white");
-  body.classList.toggle("bg-white")
+  body.classList.toggle("bg-pink-100");
+  shadowLight2.classList.toggle("shadow-black");
+  shadowLight2.classList.toggle("shadow-white");
+  shadowLight1.classList.toggle("shadow-black");
+  shadowLight1.classList.toggle("shadow-white");
+  mobileShadowLight1.classList.toggle("shadow-black");
+  mobileShadowLight1.classList.toggle("shadow-white");
+  darkBurger.classList.toggle("bg-pink-100");
+  darkBurger.classList.toggle("bg-black");
+  removeBoxPink.forEach((element) => {
+    element.classList.toggle("bg-red-200");
+    element.classList.toggle("bg-gray-500");
+  });
+  shadowLightAll.forEach((element) => {
+    element.classList.toggle("shadow-black");
+    element.classList.toggle("shadow-white");
+  });
 };
 
 btnDarkMode1.addEventListener("click", darkMode);
 btnDarkMode2.addEventListener("click", darkMode);
+
 
 
 //   MUSIQUE
